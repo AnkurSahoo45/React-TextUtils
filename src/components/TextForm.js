7e20584b0b8ed080c4c7f639aac1fe0e0b845684
AnkurSahoo45 <ankursahoo83213@gmail.com>
2024-03-28 19:05:33 +0530

    done

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -29,10 +29,10 @@ export default function TextForm(props) {
                 <h1>{props.heading}</h1>
                 <label htmlFor="myBox" className="form-label">Enter Your Text to Analyze</label>
                 <textarea className="form-control" id="myBox" value={text} onChange={handleOnChange} rows="3" style={{backgroundColor: props.mode==='dark'? 'darkcyan': 'white', color:props.mode=== 'dark'?'white': 'black'}}></textarea>
-                <button className={`btn btn-${props.mode==='dark'?'dark':'primary'} me-3 my-3`} onClick={handleUpClick}>UpperCase Text</button>
-                <button className={`btn btn-${props.mode==='dark'?'dark':'primary'} me-3 my-3`} onClick={handleLowClick}>LowerCase Text</button>
-                <button className={`btn btn-${props.mode==='dark'?'dark':'primary'} me-3 my-3`} onClick={handleClsClick}>Clear Text</button>
-                <button className={`btn btn-${props.mode==='dark'?'dark':'primary'} me-3 my-3`} onClick={handleCpyClick}>Copy Text</button>
+                <button className={`btn btn-${props.mode==='dark'?'dark':'primary'} me-3 my-3`} onClick={handleUpClick} disabled={text.length===0}>UpperCase Text</button>
+                <button className={`btn btn-${props.mode==='dark'?'dark':'primary'} me-3 my-3`} onClick={handleLowClick} disabled={text.length===0}>LowerCase Text</button>
+                <button className={`btn btn-${props.mode==='dark'?'dark':'primary'} me-3 my-3`} onClick={handleClsClick} disabled={text.length===0}>Clear Text</button>
+                <button className={`btn btn-${props.mode==='dark'?'dark':'primary'} me-3 my-3`} onClick={handleCpyClick} disabled={text.length===0}>Copy Text</button>
             </div>
             <div>
                 <h1>Your Text Summary</h1>
